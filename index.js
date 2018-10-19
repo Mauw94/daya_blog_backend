eafd5f0488a1216900a7d1e7112c476554f0afd3
Maurits Seelen <mauritsseelen@gmail.com>
2018-10-19 10:57:30 +0200

    sdfds

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ app.use('/file', fileRoutes);
 router(app)
 
 // server setup
-const port = process.env.port || 3000
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 const server = http.createServer(app)
-server.listen(port)
+server.listen(port);
 console.log('Server listening on: ', port)
